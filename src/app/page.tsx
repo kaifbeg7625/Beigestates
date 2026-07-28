af9a27638de83dab0bd16831066a7d6a083f2063
Kaif <kaif@beigestates.com>
2026-07-28 05:53:23 +0000

    Add call/enquiry contact section directly on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Story from "@/components/Story";
+import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import IntentPopup from "@/components/IntentPopup";
 import Link from "next/link";
@@ -60,6 +61,8 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <Contact />
 
       <Footer />
     </>
